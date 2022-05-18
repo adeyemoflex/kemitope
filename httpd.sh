dc8869e33f1a49d0c9d3bb4e21e9bd3c255b17c8
emmanuel adeyemo <adeyemoflex@gmail.com>
2022-05-18 14:56:43 -0400

    update my httpd file

--- httpd.sh
+++ httpd.sh
@@ -3,4 +3,4 @@ sudo su
 yum update -y
 systemctl start httpd -y
 systemctl enable httpd -y
-echo" that work well > /var/www/html/index.html
+echo" That's the way we work well > /var/www/html/index.html
